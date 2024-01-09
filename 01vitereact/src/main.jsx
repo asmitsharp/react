ab6b2c375734c8cb645bb7b53fe3ffd83bfce0a8
Asmit Singh <ashmitsingh068@gmail.com>
2024-01-09 13:44:54 +0530

    rendering custom elements in ReactDOM

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,36 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 
+function MyApp() {
+  return (
+    <div>
+      <h1>react custom APP</h1>
+    </div>
+  )
+}
+
+/*
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google.",
+}
+*/
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "visit google.com",
+  anotherUser // evaluated Expressions
+)
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <MyApp />
+  //  MyApp()
+  reactElement
 )
